0cf2fd832571e366f1234206796442867f98ebfe
adahan <adahanjolchuev@gmail.com>
2024-02-15 16:35:23 +0600

    booksshop

---
+++ src/companents/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="header">
+            <h1>fujtfyuhi</h1>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
